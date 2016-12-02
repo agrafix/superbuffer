289d50e57e2fe41da370c31de437f16653ef07dc
Alexander Thiemann <mail@athiemann.net>
2016-12-02 09:50:41 +0100

    add buffer-builder to benchmark comparison

--- bench/Bench.hs
+++ bench/Bench.hs
@@ -3,6 +3,7 @@ module Main where
 import Control.Monad
 import Data.ByteString.SuperBuffer
 import Data.Int
+import qualified Data.BufferBuilder as BB
 import qualified Data.ByteString as BS
 import qualified Data.ByteString.Lazy as BSL
 import qualified Data.ByteString.Builder as BSB
@@ -28,11 +29,13 @@ mkGroup name steps chunkSize =
     , bench (bufName iBufSize2) $ nfIO $ BS.reverse <$> buildBuf iBufSize2 steps chunkSize
     , bench (bufName iBufSize4) $ nfIO $ BS.reverse <$> buildBuf iBufSize4 steps chunkSize
     , bench (bufName iBufSizeAll) $ nfIO $ BS.reverse <$> buildBuf iBufSizeAll steps chunkSize
+    , bench (bufBBName iBufSize128) $ nfIO $ BS.reverse <$> buildBufBB iBufSize128 steps chunkSize
     , bench "bytestring builder" $ nfIO $ BS.reverse <$> buildBufBuilder steps chunkSize
     , bench "bytestring fromChunks" $ nfIO $ BS.reverse <$> buildBufChunks steps chunkSize
     , bench "bytestring concat" $ nfIO $ BS.reverse <$> buildBufConcat steps chunkSize
     ]
     where
+      bufBBName is = "buffer-builder (init=" ++ show is ++ " bytes, trim=yes)"
       bufName is = "superbuffer (init=" ++ show is ++ " bytes)"
       iBufSize128 = 128
       iBufSize = fromIntegral chunkSize
@@ -50,6 +53,18 @@ buildBuf bufSize steps chunkSize =
     withBuffer bufSize $ \buf ->
     forM_ [0..steps] $ \step ->
     appendBuffer buf (mkChunk step chunkSize)
+
+buildBufBB :: Int64 -> Int -> Int -> IO BS.ByteString
+buildBufBB bufSize steps chunkSize =
+    pure $ BB.runBufferBuilderWithOptions opts $
+    forM_ [0..steps] $ \step ->
+    BB.appendBS (mkChunk step chunkSize)
+    where
+      opts =
+          BB.Options
+          { BB.initialCapacity = fromIntegral bufSize
+          , BB.trimFinalBuffer = True
+          }
 
 buildBufBuilder :: Int -> Int -> IO BS.ByteString
 buildBufBuilder steps chunkSize =
